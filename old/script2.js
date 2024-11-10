b1fa7f94bba9fc309ace0736bb4572d7aae31666
dmitry <dmitry@antonyuk.org>
2024-11-10 22:06:11 +0200

    вав

--- old/script2.js
+++
@@ -1,7 +0,0 @@
-function scrollMeTo(event,elementId) {
-    event.preventDefault()
-    const element = document.getElementById(elementId);
-    if (element) {
-      element.scrollIntoView({ block: "center", behavior: "smooth" });
-    }
-  }
