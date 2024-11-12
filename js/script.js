32ea24a63f2178157087ac38af5b6f1ad56a05e8
borntojesus <borntojesus@gmail.com>
2024-11-12 22:30:35 +0200

    page update

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ function toggleMenu() {
   burgerMenu.classList.toggle("active");
   mobileMenu.classList.toggle("active");
 }
+
+
+$(document).ready(function () {
+  const $slider = $('.services__slider');
+  
+  $slider.slick({
+      slidesToShow: 2, 
+      slidesToScroll: 2, 
+      infinite: false, 
+      arrows: true,
+      dots: true,
+      customPaging: function (slider, i) {
+       
+          return '<button class="custom-dot"></button>';
+      },
+      responsive: [
+          {
+              breakpoint: 768,
+              settings: {
+                  slidesToShow: 1, 
+                  slidesToScroll: 1,
+                  dots: true
+              }
+          }
+      ]
+  });
+});
